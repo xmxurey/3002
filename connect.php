c0c8825de50be29e3969e3bdbd8c19171d3f185e
ZoeQi <qiyiru@gmail.com>
2014-10-17 22:43:20 +0800

    database name

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $server = "localhost";  
 $username = "root"; 
 $password = "root"; 
-$database = "my_db"; 
+$database = "cz3002"; 
 
 mysql_connect("$server", "$username", "$password") or die(mysql_error()); 
 mysql_select_db("$database") or die(mysql_error()); 
